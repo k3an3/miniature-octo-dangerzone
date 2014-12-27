f1c466775ce0608b338ae2ef0a3c946ff27dccd8
Keane O'Kelley <korockinout13@gmail.com>
2014-12-26 21:49:12 -0600

    Progress

--- issues/urls.py
+++ issues/urls.py
@@ -1,4 +1,8 @@
 from django.conf.urls import patterns, url
 from issues import views
 
-urlpatterns = patterns('', url(r'^$', views.index, name='index'),)
+urlpatterns = patterns('',
+    url(r'^$', views.index, name='index'),
+    url(r'^(?P<issue_id>\d+)/$', views.issueDetail, name='detail'),
+    url(r'^(?P<issue_id>\d+)/vote/$', views.vote, name='vote'),
+    )
--- issues/views.py
+++ issues/views.py
@@ -1,6 +1,22 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from issues.models import Issue
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect, HttpResponse
 
 def index(request):
-    return HttpResponse("Welcome to mOctoD")
+    issue_recent_list = Issue.objects.order_by('-issue_date')[:25]
+    context = {'issue_recent_list' : issue_recent_list}
+    return render(request, 'issues/index.html', context)
 
+def issueDetail(request, issue_id):
+    issue = get_object_or_404(Issue, pk=issue_id)
+    return render(request, 'issues/detail.html', {'issue' : issue})
+
+def vote(request, issue_id):
+    issue = get_object_or_404(Issue, pk=issue_id)
+    if request.POST.get('voteup', False):
+        issue.issue_votes += 1
+    elif request.POST.get('votedown', False):
+        issue.issue_votes -= 1
+    issue.save()
+    return HttpResponseRedirect(reverse('issues:detail', args=(issue.id,)))
