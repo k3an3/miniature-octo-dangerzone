e342dd63d8b11bb6fb4c2c7556269f2a598d9a7e
Keane O'Kelley <korockinout13@gmail.com>
2015-08-18 19:01:30 +0000

    Allow local settings

--- octo/settings.py
+++ octo/settings.py
@@ -7,7 +7,6 @@ https://docs.djangoproject.com/en/1.7/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.7/ref/settings/
 """
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
@@ -87,3 +86,8 @@ STATIC_URL = '/static/'
 TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
 LOGIN_REDIRECT_URL = '/'
 LOGIN_URL = '/accounts/login'
+
+try:
+    from settings_local import *
+except ImportError:
+    pass
