1c76f363744f9a1ab86cdcb8c9cd647d094a2f3f
Keane O'Kelley <korockinout13@gmail.com>
2015-08-20 14:46:07 -0500

    Migration

---
+++ issues/migrations/0020_auto_20150820_1434.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import datetime
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('issues', '0019_auto_20150818_1318'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='issue',
+            name='date',
+            field=models.DateTimeField(default=datetime.datetime(2015, 8, 20, 14, 34, 41, 503739)),
+        ),
+    ]
